5eff71ebf15377fabf7b864de1602fb94c8fcc74
choiseona <tjsdk617@naver.com>
2024-01-13 02:40:32 +0900

    FEAT: tvNEnjoystories 폰트 세팅

--- src/pages/MainHome/index.tsx
+++ src/pages/MainHome/index.tsx
@@ -3,6 +3,7 @@ const MainHome = () => {
     <>
       <div className="test2">Pretendard test</div>
       <div className="text-red-400 test tracking-tighter">gfgfgfgfgfdfdf</div>
+      <div className="test3"> ggg</div>
     </>
   );
 };
