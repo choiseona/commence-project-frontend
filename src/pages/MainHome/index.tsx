5a78cc4192398e3e9fef174eb32e5ea7d4271209
sockki <poohct327@gmail.com>
2024-01-12 17:37:44 +0900

    FEAT: 전체 크기 조절 및 layout 컴포넌트 추가 - #4

--- src/pages/MainHome/index.tsx
+++ src/pages/MainHome/index.tsx
@@ -2,7 +2,7 @@ const MainHome = () => {
   return (
     <>
       <div className="test2">Pretendard test</div>
-      <div className="text-red-400 test tracking-tighter">gfgfgfgfg</div>
+      <div className="text-red-400 test tracking-tighter">gfgfgfgfgfdfdf</div>
     </>
   );
 };
