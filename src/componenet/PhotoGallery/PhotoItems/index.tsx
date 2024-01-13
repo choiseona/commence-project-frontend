0a1b9aeeca1ac0f72e5920ae737d0d5574483938
choiseona <tjsdk617@naver.com>
2024-01-13 14:19:33 +0900

    REFACTOR: PhotoGalleryData data 파일로 이동 - #6

--- src/componenet/PhotoGallery/PhotoItems/index.tsx
+++ src/componenet/PhotoGallery/PhotoItems/index.tsx
@@ -1,129 +1,6 @@
 import { Fragment } from "react";
-import testImage from "@/assets/images/photo-gallery-test1.jpg";
 import { PhotoItem } from "../PhotoItem";
-
-const datas = {
-  January: {
-    items: [
-      {
-        src: testImage,
-        alt: "겨울MT 사진",
-        content: "겨울MT",
-      },
-      {
-        src: testImage,
-        alt: "신년회 사진",
-        content: "신년회",
-      },
-    ],
-  },
-
-  February: {
-    items: [
-      {
-        src: testImage,
-        alt: "꼬망서 졸업식 사진",
-        content: "꼬망서 졸업식",
-      },
-    ],
-  },
-  March: {
-    items: [
-      {
-        src: testImage,
-        alt: "OB x YB 꼬망서 순수창작가요 발매 사진",
-        content: "OBxYB 꼬망서\n순수창작가요 발매",
-      },
-      {
-        src: testImage,
-        alt: "동아리 박람회 사진",
-        content: "동아리 박람회",
-      },
-      {
-        src: testImage,
-        alt: "2023 꼬망스 소음악회 사진",
-        content: "2023 꼬망스 소음악회",
-      },
-    ],
-  },
-  April: {
-    items: [
-      {
-        src: testImage,
-        alt: "IndkyxAssessxCOMMENCE\n연합공연 사진",
-        content: "IndkyxAssessxCOMMENCE\n연합공연",
-      },
-    ],
-  },
-  May: {
-    items: [
-      {
-        src: testImage,
-        alt: "2023 비룡제 사진",
-        content: "2023 비룡제",
-      },
-      {
-        src: testImage,
-        alt: "통일광장 야외공연 사진",
-        content: "통일광장 야외공연",
-      },
-    ],
-  },
-  July: {
-    items: [
-      {
-        src: testImage,
-        alt: "클럽공연 사진",
-        content: "클럽공연",
-      },
-    ],
-  },
-  August: {
-    items: [
-      {
-        src: testImage,
-        alt: "인하대 4대 밴드 연합공연 in 롤링홀 사진",
-        content: "인하대 4대 밴드\n연합공연 in 롤링홀",
-      },
-    ],
-  },
-  September: {
-    items: [
-      {
-        src: testImage,
-        alt: "연합 단과대 소축제 공연 사진",
-        content: "연합 단과대\n소축제 공연",
-      },
-    ],
-  },
-  October: {
-    items: [
-      {
-        src: testImage,
-        alt: "YB 순수창작가요 발매 사진",
-        content: "YB 순수창작가요 발매",
-      },
-    ],
-  },
-  November: {
-    items: [
-      {
-        src: testImage,
-        alt: "꼬망스 정기공연 사진",
-        content: "꼬망스 정기공연",
-      },
-    ],
-  },
-  December: {
-    items: [
-      {
-        src: testImage,
-        alt: "2023 종강총회 사진",
-        content: "2023 종강총회",
-      },
-    ],
-  },
-};
+import { photoGalleryData } from "@/data/PhotoGalleryData";
 
 function PhotoItems() {
   const marginBottomVariants: { [key: string]: string } = {
@@ -142,7 +19,7 @@ function PhotoItems() {
 
   return (
     <>
-      {Object.entries(datas).map(([month, { items }]) => (
+      {Object.entries(photoGalleryData).map(([month, { items }]) => (
         <Fragment key={month}>
           {items.map(({ src, alt, content }, index) => (
             <PhotoItem key={index} imageKey={`${month}_${index + 1}`}>
