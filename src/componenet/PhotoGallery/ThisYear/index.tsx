067e307771efc9865b12f8b0ffb358c30c58a880
choiseona <tjsdk617@naver.com>
2024-01-13 13:15:51 +0900

    FEAT: CommenceMark 컴포넌트 완성 - #6

--- src/componenet/PhotoGallery/ThisYear/index.tsx
+++ src/componenet/PhotoGallery/ThisYear/index.tsx
@@ -1,6 +1,6 @@
 function ThisYear() {
   return (
-    <p className="text-center font-tvNEnjoystoriesB text-[40px] leading-[100%] tracking-[-0.4px]">
+    <p className="font-tvNEnjoystoriesB text-[40px] leading-[100%] tracking-[-0.4px]">
       COMMENCE's 2023
     </p>
   );
--- src/pages/PhotoGallery/index.tsx
+++ src/pages/PhotoGallery/index.tsx
@@ -1,18 +1,20 @@
 import PhotoLine from "@/componenet/PhotoGallery/PhotoLine";
 import ThisYear from "@/componenet/PhotoGallery/ThisYear";
 import PhotoItems from "@/componenet/PhotoGallery/PhotoItems";
+import CommenceMark from "@/componenet/PhotoGallery/CommenceMark";
 
 function PhotoGallery() {
   return (
-    <div className="pt-[43px]">
+    <div className="pt-[43px] flex flex-col items-center">
       <ThisYear />
-      <div className="mt-[166px] flex justify-center">
-        <div className="relative">
-          <PhotoLine />
-          <div className="absolute top-[-84.5px] left-[-74px]">
-            <PhotoItems />
-          </div>
+      <div className="mt-[166px] relative">
+        <PhotoLine />
+        <div className="absolute top-[-84.5px] left-[-74px]">
+          <PhotoItems />
         </div>
+      </div>
+      <div className="mt-[34.5px]">
+        <CommenceMark />
       </div>
     </div>
   );
