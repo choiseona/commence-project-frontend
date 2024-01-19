36772c7781be71b092046c2a14a310f416c464fd
sockki <poohct327@gmail.com>
2024-01-19 12:46:46 +0900

    Footer em => %, px => rem 단위 변경 - #15

--- src/componenet/Common/Footer/index.tsx
+++ src/componenet/Common/Footer/index.tsx
@@ -5,7 +5,7 @@ import DeveloperButton from "../DeveloperButton";
 
 function Footer() {
   return (
-    <div title="footer" className="pt-[18px] pb-[10px] relative">
+    <div title="footer" className="pt-[1.8rem] pb-[1rem] relative">
       <DeveloperButton />
       <FooterTitle />
       <FooterText />
