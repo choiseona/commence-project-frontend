244286e7b222817dfb3d9e0d347a5afd72c488c3
choiseona <tjsdk617@naver.com>
2024-01-21 03:54:29 +0900

    FEAT: CommenceMark 컴포넌트 완성 - #16

---
+++ src/componenet/Common/CommonMark/index.tsx
@@ -0,0 +1,6 @@
+import commenceMark from "@/assets/icons/commence-mark.svg";
+function CommenceMark() {
+  return <img src={commenceMark} alt="꼬망스 마크" className="w-[36.7rem]" />;
+}
+
+export default CommenceMark;
