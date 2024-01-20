e336fef0904afd9d500bba80bdcea05c7763e95d
choiseona <tjsdk617@naver.com>
2024-01-21 01:49:30 +0900

    FEAT: SecondAuditionExplaination 컴포넌트 완성 - #16

---
+++ src/componenet/HowToJoin/SecondAuditionExplaination/index.tsx
@@ -0,0 +1,28 @@
+function SecondAuditionExplaination() {
+  return (
+    <div>
+      <div className="flex items-center gap-[0.739rem]">
+        <div className="w-[0.522rem] h-[0.522rem] bg-black rounded-full" />
+        <p className="font-tvNEnjoystoriesB text-[2rem] leading-[100%] tracking-[-0.02rem]">
+          2차 오디션
+        </p>
+      </div>
+
+      <p className="mt-[0.4rem] font-AppleSDGothicNeoL00 text-[1.3rem] leading-_normal">
+        1차 오디션과 마찬가지로, 본인이 준비한{" "}
+        <span className="font-AppleSDGothicNeoB00">노래 두 곡을 1절 분량</span>
+        으로
+        <br />
+        가창 또는 연주와{" "}
+        <span className="font-AppleSDGothicNeoB00">질의응답</span>을 진행합니다.
+        <br />
+        <br /> 질의응답 시간 이후 지원자분의 각오를 듣는{" "}
+        <span className="font-AppleSDGothicNeoB00">짧은 자기 PR 시간</span>이
+        <br />
+        있습니다. 부담 갖지 말고 간단히 준비하시면 됩니다.
+      </p>
+    </div>
+  );
+}
+
+export default SecondAuditionExplaination;
