e360269026e7482db28bb6d9e092c6a1ca6ddb49
sockki <poohct327@gmail.com>
2024-01-21 16:44:24 +0900

    FIX: 제작자들 버튼 화살표 svg로 교체

--- src/componenet/Common/DeveloperButton/index.tsx
+++ src/componenet/Common/DeveloperButton/index.tsx
@@ -1,9 +1,14 @@
+import rightarrow from "@/assets/icons/right-arrow-icon.svg"
+
 function DeveloperButton() {
   return (
-    <button className="absolute top-[3rem] right-[2.2rem] bg-black  flex justify-center items-center w-[12%] h-[10%] rounded-2xl">
-      <span className="text-white font-AppleSDGothicNeoL00 text-[10%] mt-[0.3rem]">
-        제작자 →
-      </span>
+    <button className="absolute top-[3rem] right-[2.2rem] bg-black  flex justify-center items-center w-[12%] h-[10%] rounded-[1.5rem]">
+      <div className="text-white font-AppleSDGothicNeoL00 text-[10%] mt-[0.3rem] flex items-center space-x-2">
+        <span>제작자</span>
+        <span className="mb-[0.2rem]">
+          <img src={rightarrow} alt="right-arrow"/>
+        </span>
+      </div>
     </button>
   );
 }
