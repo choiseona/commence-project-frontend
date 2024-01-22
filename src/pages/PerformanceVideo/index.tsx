323915b1ca28f20bb3ddad7fe2df77433c2870ba
choiseona <tjsdk617@naver.com>
2024-01-22 22:37:11 +0900

    FEAT: videoItem 컴포넌트 완성 - #23

--- src/pages/PerformanceVideo/index.tsx
+++ src/pages/PerformanceVideo/index.tsx
@@ -1,11 +1,13 @@
 import Title from "@/componenet/Common/Title";
 import SubTitle from "@/componenet/PerformanceVideo/SubTitle";
+import VideoItem from "@/componenet/PerformanceVideo/VideoItem";
 
 function PerformanceVideo() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Title content="Best Performance" />
       <SubTitle />
+      <VideoItem />
     </div>
   );
 }
