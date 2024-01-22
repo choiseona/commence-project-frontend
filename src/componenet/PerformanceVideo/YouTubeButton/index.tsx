d18975c6823631144c0580d7c0c55d93f6cf0122
choiseona <tjsdk617@naver.com>
2024-01-22 23:50:48 +0900

    FEAT: 꼬망스 유튜브 채널 링킹 - #23

--- src/componenet/PerformanceVideo/YouTubeButton/index.tsx
+++ src/componenet/PerformanceVideo/YouTubeButton/index.tsx
@@ -1,8 +1,9 @@
 import youtubeMark from "@/assets/icons/youtube-redmark.svg";
 
 function YoutubeButton() {
+  const COMMENCE_YOUTUBE_CHANNEL = "https://www.youtube.com/@commence-sw6xo";
   const onClick = () => {
-    window.location.href = "";
+    window.location.href = COMMENCE_YOUTUBE_CHANNEL;
   };
 
   return (
