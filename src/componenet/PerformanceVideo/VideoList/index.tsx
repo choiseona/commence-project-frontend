4c885a5253bbc631acd2ecebfd5ddf644b5d9488
choiseona <tjsdk617@naver.com>
2024-01-22 23:03:33 +0900

    FEAET: VideoList 컴포넌트 완성 - #23

---
+++ src/componenet/PerformanceVideo/VideoList/index.tsx
@@ -0,0 +1,18 @@
+import { PerformanceVideoData } from "@/data/PerformanceVideoData";
+import { VideoItem } from "../VideoItem";
+
+function VideoList() {
+  return (
+    <div className="grid grid-cols-2 gap-x-[1.172rem] gap-y-[0.8rem]">
+      {PerformanceVideoData.map((video, index) => (
+        <VideoItem key={index}>
+          <VideoItem.Genre genre={video.genre} />
+          <VideoItem.Thumbnail imgSrc={video.imgSrc} alt={video.alt} />
+          <VideoItem.Information title={video.title} singer={video.singer} />
+        </VideoItem>
+      ))}
+    </div>
+  );
+}
+
+export default VideoList;
---
+++ src/data/PerformanceVideoData.ts
@@ -0,0 +1,60 @@
+import test from "@/assets/images/test.png";
+
+export const PerformanceVideoData = [
+  {
+    genre: "세션곡",
+    imgSrc: test,
+    alt: "2023 정기공연 Hero 유튜브 영상 thumbnail",
+    title: "2023 정기공연 Hero",
+    singer: "Kaz rodriguez",
+  },
+  {
+    genre: "발라드",
+    imgSrc: test,
+    alt: "2022 소음악회 Promise you 유튜브 영상 thumbnail",
+    title: "2022 소음악회 Promise you",
+    singer: "브라운 아이드 소울",
+  },
+  {
+    genre: "Rock",
+    imgSrc: test,
+    alt: "2022 정기공연 사랑한 후에 유튜브 영상 thumbnail",
+    title: "2022 정기공연 사랑한 후에",
+    singer: "하동균",
+  },
+  {
+    genre: "Dance",
+    imgSrc: test,
+    alt: "2023 비룡제 야외공연 Swing baby 유튜브 영상 thumbnail",
+    title: "2023 비룡제 야외공연 Swing baby",
+    singer: "울랄라 세션",
+  },
+  {
+    genre: "K-pop",
+    imgSrc: test,
+    alt: "2023 통일광장 야외공연 다시 만난 세계 유튜브 영상 thumbnail",
+    title: "2023 통일광장 야외공연 다시 만난 세계",
+    singer: "소녀시대",
+  },
+  {
+    genre: "창작곡",
+    imgSrc: test,
+    alt: "2023 정기공연 Shadow 유튜브 영상 thumbnail",
+    title: "2023 정기공연 Shadow",
+    singer: "꼬망스",
+  },
+  {
+    genre: "RnB",
+    imgSrc: test,
+    alt: "2023 비룡제 야외공연 파렴치 유튜브 영상 thumbnail",
+    title: "2023 비룡제 야외공연 파렴치",
+    singer: "YEGNY",
+  },
+  {
+    genre: "Pop",
+    imgSrc: test,
+    alt: "2023 정기공연 Man in the mirror 유튜브 영상 thumbnail",
+    title: "2023 정기공연 Man in the mirror",
+    singer: "Michael Jackson",
+  },
+];
