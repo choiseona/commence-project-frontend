f36a76c64f5d438069e4a790127f51171af5de13
sockki <poohct327@gmail.com>
2024-01-22 20:35:58 +0900

    FEAT: MovingBanner 컴포넌트 완성, SlideBanner 텍스트 완성

--- src/componenet/MainHome/SlideBanner/index.tsx
+++ src/componenet/MainHome/SlideBanner/index.tsx
@@ -104,6 +104,18 @@ function SlideBanner() {
         className="relative h-[26.2rem] w-full flex justify-center items-center"
         ref={outRef}
       >
+        <div className="absolute z-[4] flex flex-col items-center top-[3rem]">
+          <span className="font-AppleSDGothicNeoSB00 text-[3rem]">
+            Who is Next?
+          </span>
+          <span className="font-AppleSDGothicNeoL00 text-[1.4rem] text-[#6E6E6E] -mt-[0.3rem]">
+            인하대 No.1 밴드 동아리{" "}
+            <span className="font-AppleSDGothicNeoSB00 text-[1.4rem] text-black">
+              꼬망스
+            </span>
+            에서 24학번 신입부원을 기다립니다.
+          </span>
+        </div>
         <AnimatePresence initial={false} custom={direction}>
           <motion.div
             className="bg-gray-300 w-full h-full absolute top-0 flex items-center justify-center"
@@ -120,7 +132,7 @@ function SlideBanner() {
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
+            onDragEnd={(_e, { offset, velocity }) => {
               const swipe = swipePower(offset.x, velocity.x);
 
               if (swipe < -swipeConfidenceThreshold) {
