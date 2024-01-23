93b6f2e2e9231e01baff4a4969028bf1b9eed203
sockki <poohct327@gmail.com>
2024-01-23 16:40:09 +0900

    FIX: Menu 컴포넌트 overlay클릭시 메뉴 닫히도록 수정

--- src/componenet/MainHome/MovingBanner/index.tsx
+++ src/componenet/MainHome/MovingBanner/index.tsx
@@ -2,7 +2,7 @@ function MovingBanner() {
   return (
     <div className="relative w-full max-w-[100%] h-auto py-[1rem] overflow-x-hidden overflow-y-hidden bg-black">
       <div className="absolute whitespace-nowrap will-change-[transform] animate-[marquee_30s_linear_infinite]">
-        <div className="text-[1rem] text-white -translate-y-[0.8rem] font-Pretendard">
+        <div className="text-[1rem] text-white -translate-y-1/2 font-Pretendard">
         &nbsp;&nbsp;2024 COMMENCER &nbsp;♬&nbsp; Vocal&nbsp; ♬&nbsp; Keyboard&nbsp; ♬&nbsp; Electric guitar&nbsp; ♬&nbsp; Bass guitar&nbsp; ♬
           &nbsp;Acoustic guitar&nbsp; ♬&nbsp; Drum&nbsp; ♬&nbsp; MIDI&nbsp;&nbsp;♬&nbsp;&nbsp;2024 COMMENCER &nbsp;♬&nbsp; Vocal&nbsp; ♬&nbsp; Keyboard&nbsp; ♬&nbsp; Electric guitar&nbsp; ♬&nbsp; Bass guitar&nbsp; ♬
           &nbsp;Acoustic guitar&nbsp; ♬&nbsp; Drum&nbsp; ♬&nbsp; MIDI
