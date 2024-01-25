1431710a9b5d84b777120f1eee843923568b318a
sockki <poohct327@gmail.com>
2024-01-25 17:31:06 +0900

    FEAT: Awards 컴포넌트 완성

--- src/componenet/AboutUs/Awards/index.tsx
+++ src/componenet/AboutUs/Awards/index.tsx
@@ -1,5 +1,25 @@
+import { AwardsData } from "@/data/AwardsData";
+import AboutUsSubTitle from "../AboutUsSubTitle";
+import AwardsItem from "../AwardsItem";
+import AwardsLine from "../AwardsLine";
+
 function Awards() {
-    return <div></div>
+  return (
+    <div className="mt-[2.57rem]">
+      <AboutUsSubTitle main="꼬망스 수상 내역" sub="est.1984" />
+      <div className="ml-[4.31rem] mt-[2.5rem] relative space-y-[0.3rem] pb-[7rem]">
+      <AwardsLine />
+        {AwardsData.map((data, index) => (
+          <AwardsItem
+            key={index}
+            color={data.color}
+            year={data.year}
+            awards={data.awards}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- src/componenet/AboutUs/MainPerformances/index.tsx
+++ src/componenet/AboutUs/MainPerformances/index.tsx
@@ -4,7 +4,7 @@ import PerformanceBox from "../PerformanceBox";
 
 function MainPerformances() {
   return (
-    <div className="mt-[6.57rem] flex flex-col items-center">
+    <div className="mt-[6.57rem] flex flex-col">
       <AboutUsSubTitle main="Main performances" sub="throughout the year"/>
       <div className="mt-[2.5rem] mx-[9.65%] grid grid-rows-4 space-y-[1rem] self-stretch items-center">
         {MainPerformacesData.map((data,index) => <PerformanceBox key={index} month={data.month} show={data.show} contents={data.contents} color={data.color}/>)}
