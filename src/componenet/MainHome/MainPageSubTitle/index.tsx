1dbf5f2c35547e266b3c7030ef3954ec29f52d36
sockki <poohct327@gmail.com>
2024-01-30 15:29:01 +0900

    FIX: text-black 추가

--- src/componenet/MainHome/MainPageSubTitle/index.tsx
+++ src/componenet/MainHome/MainPageSubTitle/index.tsx
@@ -5,10 +5,10 @@ interface SubTitleProps {
 
 function MainPageSubTitle({mainText,subText}:SubTitleProps){
     return <div className="ml-[0.373rem] mb-[0.7rem] flex flex-col">
-    <span className="font-AppleSDGothicNeoH00 text-[1.5rem]">
+    <span className="font-AppleSDGothicNeoH00 text-black text-[1.5rem]">
       {mainText}
     </span>
-    <span className="font-AppleSDGothicNeoL00 text-[1rem] leading-[100%] tracking-[0.04rem]">
+    <span className="font-AppleSDGothicNeoL00 text-black text-[1rem] leading-[100%] tracking-[0.04rem]">
       {subText}
     </span>
   </div>
