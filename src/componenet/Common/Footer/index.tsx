635df19d56292a67b25c611308c9ab5f3c562208
sockki <poohct327@gmail.com>
2024-02-02 19:05:11 +0900

    FIX: Footer에 개발자들 버튼 임시 삭제

--- src/componenet/Common/Footer/index.tsx
+++ src/componenet/Common/Footer/index.tsx
@@ -1,12 +1,11 @@
 import FooterTitle from "../FooterTitle";
 import FooterText from "../FooterText";
 import FooterLinks from "../FooterLinks";
-import DeveloperButton from "../DeveloperButton";
+
 
 function Footer() {
   return (
     <div title="footer" className="pt-[1.8rem] pb-[1rem] relative bg-white">
-      <DeveloperButton />
       <FooterTitle />
       <FooterText />
       <FooterLinks />
