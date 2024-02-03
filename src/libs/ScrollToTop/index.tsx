3d86c125dffc503861965c944fcdd3ba13591091
sockki <poohct327@gmail.com>
2024-02-04 00:31:30 +0900

    FIX: ScrollToTop 애니메이션 제거 - #46

--- src/libs/ScrollToTop/index.tsx
+++ src/libs/ScrollToTop/index.tsx
@@ -5,7 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo(0,0);
   }, [pathname]);
 
   return null;
