3d253d328a7242c5d327ff864ad8be5bd72fd161
sockki <poohct327@gmail.com>
2024-02-19 14:55:18 +0900

    FIX: About Commence SEO 개선

--- src/componenet/AboutUs/AwardsLine/index.tsx
+++ src/componenet/AboutUs/AwardsLine/index.tsx
@@ -4,10 +4,18 @@ import smallDat from "@/assets/icons/small-dat.svg";
 function AwardsLine() {
   return (
     <div className="absolute left-[0.7rem] top-[0] ">
-      <img src={awardsLine} />
-      <img className="-ml-[0.255rem] mt-[1rem]" src={smallDat} />
-      <img className="-ml-[0.255rem] -mt-[0.3rem]" src={smallDat} />
-      <img className="-ml-[0.255rem] -mt-[0.3rem]" src={smallDat} />
+      <img src={awardsLine} alt="awardsLine" />
+      <img className="-ml-[0.255rem] mt-[1rem]" src={smallDat} alt="smallDat" />
+      <img
+        className="-ml-[0.255rem] -mt-[0.3rem]"
+        src={smallDat}
+        alt="smallDat"
+      />
+      <img
+        className="-ml-[0.255rem] -mt-[0.3rem]"
+        src={smallDat}
+        alt="smallDat"
+      />
     </div>
   );
 }
