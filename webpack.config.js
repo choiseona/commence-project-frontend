55464eaec7313f19a1a12a64766629b44ad41a99
sockki <poohct327@gmail.com>
2024-02-23 14:09:05 +0900

    FIX: Gzip compressing 추가

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     // 기존 플러그인 설정
     new CompressionWebpackPlugin({
       algorithm: 'gzip',
-      test: /\.(js|css|html)$/,
+      test: /\.(ts|tsx|js|css|html)$/,
       threshold: 10240, // 압축을 적용할 파일 크기의 최소값 (10KB)
       minRatio: 0.8, // 압축률이 80% 이상일 경우에만 압축을 적용
     }),
